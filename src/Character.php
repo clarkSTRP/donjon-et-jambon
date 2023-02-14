5042ff03afe97e8c7af45b97fdd0c5b42cfdf9a9
Jhinzu <jeandetorres30140@gmail.com>
2023-02-14 11:05:58 +0100

    push fonction soin armure

--- src/Character.php
+++ src/Character.php
@@ -6,15 +6,32 @@ class Character {
     public $health;
     public $attack;
     public $name;
+    public $armor;
 
-   public function __construct($name,$health,$attack)
+   public function __construct($name,$health,$attack,$armor)
     {
         $this->name = $name;
         $this->health = $health;
         $this->attack = $attack;
+        $this->armor = $armor;
     }
-    public function Dodamage($target){
-        this->$target($health) =  
-        
+
+    // potion de soin + 50hp
+    public function addSoin()
+    {
+        $this->health = $this->health + 50;
     }
+    
+    // ajout armure +20
+    public function addArmor()
+    {
+        $this->armor = $this->armor + 20;
+    }
+
+    //attack perso vs perso 
+    public function DoDamage($target){
+        $target->health -= $this->attack;
+        dump($target);
+    }
+    
 }
