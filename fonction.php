1c6c1bc7fbbaf1ede9da211425c1e741f102c6be
Jhinzu <jeandetorres30140@gmail.com>
2023-02-14 15:09:25 +0100

    ajout atk aléatoire

--- fonction.php
+++ fonction.php
@@ -11,11 +11,11 @@ function writeText($value) {
  $Hero = new Character($playerName,200,15,0);
  $Hero->addSoin();
  dump($Hero);
- $SmallMinion = new Character("Small-Minion",10,10,0);
+ $SmallMinion = new Character("Small-Minion",10,rand(0,10),0);
  dump($SmallMinion);
- $MediumMinion = new Character("Medium-Minion",30,30,0);
+ $MediumMinion = new Character("Medium-Minion",30,rand(5,30),0);
  dump($MediumMinion);
- $BigMinion = new Character("Big-Minion",100,100,0);
+ $BigMinion = new Character("Big-Minion",100,rand(30,100),0);
  dump($BigMinion);
 
  $Hero->DoDamage($SmallMinion);
