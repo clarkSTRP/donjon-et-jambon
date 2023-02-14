a4f2d7b097f7b9987fef3b5fdbf24451cefeab7a
Jhinzu <jeandetorres30140@gmail.com>
2023-02-14 16:23:20 +0100

    ajout d'unpeu de style sur l'intro

--- intro.php
+++ intro.php
@@ -12,53 +12,56 @@ $name = $_GET["playerName"];
     <title>Intro</title>
 </head>
 <body>
-    <pre>
-                 ."  ."      ".  ".
-                ;  ."    /\    ".  ;
-                ;  '._,-/  \-,_.`  ;
-                \  ,`  / /\ \  `,  /
-                 \/    \/  \/    \/
-                 ,=_    \/\/    _=,
-                 |  "_   \/   _"  |
-                 |_   '"-..-"'   _|
-                 | "-.        .-" |
-                 |    "\    /"    |
-                 |      |  |      |
-         ___     |      |  |      |     ___
-     _,-",  ",   '_     |  |     _'   ,"  ,"-,_
-   _(  \  \   \"=--"-.  |  |  .-"--="/   /  /  )_
- ,"  \  \  \   \      "-'--'-"      /   /  /  /  ".
-!     \  \  \   \                  /   /  /  /     !
-:      \  \  \   \                /   /  /  /      
-    </pre>
+    <main class="introMain">
+        <pre>
+                    ."  ."      ".  ".
+                    ;  ."    /\    ".  ;
+                    ;  '._,-/  \-,_.`  ;
+                    \  ,`  / /\ \  `,  /
+                    \/    \/  \/    \/
+                    ,=_    \/\/    _=,
+                    |  "_   \/   _"  |
+                    |_   '"-..-"'   _|
+                    | "-.        .-" |
+                    |    "\    /"    |
+                    |      |  |      |
+            ___     |      |  |      |     ___
+        _,-",  ",   '_     |  |     _'   ,"  ,"-,_
+    _(  \  \   \"=--"-.  |  |  .-"--="/   /  /  )_
+    ,"  \  \  \   \      "-'--'-"      /   /  /  /  ".
+    !     \  \  \   \                  /   /  /  /     !
+    :      \  \  \   \                /   /  /  /      
+        </pre>
 
-    <p>Welcome to Donjon and Jambon, an exhilarating adventure game where you play the role of a brave adventurer who must traverse a treacherous dungeon filled with traps, monsters,
-        and hidden treasures. Your mission is to defeat the enemies that stand in your path, solve complex puzzles, and find the keys that will allow you to open the doors of the dungeon and reach the treasure room. But beware, each level of the dungeon is increasingly difficult, 
-        so be sure to be well-equipped and prepared to face all the challenges that await you. Are you ready to take on the challenge? Enter the dungeon and begin your adventure now!</p>
+        <p class="introText">Welcome to Donjon and Jambon, an exhilarating adventure game where you play the role of a brave adventurer who must traverse a treacherous dungeon filled with traps, monsters,
+            and hidden treasures. Your mission is to defeat the enemies that stand in your path, solve complex puzzles, and find the keys that will allow you to open the doors of the dungeon and reach the treasure room. But beware, each level of the dungeon is increasingly difficult, 
+            so be sure to be well-equipped and prepared to face all the challenges that await you. Are you ready to take on the challenge? Enter the dungeon and begin your adventure now!</p>
 
-    <pre>⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢹⣿⣿
-⣿⣿⣿⣿⣿⣛⡛⠛⠿⠿⠿⢿⣿⣿⣿⣿⣿⡿⠿⠟⠛⠿⣿⡟⠃⠀⠀⠀⢹⣿
-⣿⣿⠟⠉⠉⠉⠛⠳⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⣶⣿⣤⣤⣼⣿
-⣿⡏⠀⠀⠀⠀⠀⠀⠈⠻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿
-⣿⡇⠀⠀⢀⣤⣄⠀⠀⠀⠹⡆⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⡇⠀⠀⠸⣿⣿⣧⠀⠀⠀⣿⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⡀⠀⠀⠙⠛⠋⠀⠀⠀⣿⡇⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⣿⠁⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣦⣀⠀⠀⠀⢀⣴⣏⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</pre>
+<pre>
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢹⣿⣿
+    ⣿⣿⣿⣿⣿⣛⡛⠛⠿⠿⠿⢿⣿⣿⣿⣿⣿⡿⠿⠟⠛⠿⣿⡟⠃⠀⠀⠀⢹⣿
+    ⣿⣿⠟⠉⠉⠉⠛⠳⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⣶⣿⣤⣤⣼⣿
+    ⣿⡏⠀⠀⠀⠀⠀⠀⠈⠻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿
+    ⣿⡇⠀⠀⢀⣤⣄⠀⠀⠀⠹⡆⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⡇⠀⠀⠸⣿⣿⣧⠀⠀⠀⣿⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⡀⠀⠀⠙⠛⠋⠀⠀⠀⣿⡇⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⣿⠁⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣦⣀⠀⠀⠀⢀⣴⣏⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</pre>
 
-<p>here is your Sword <span class="namePlayer"><?=$name?></span> good luck...</p>
+    <p>here is your Sword <span class="namePlayer"><?=$name?></span> good luck...</p>
 
 
-<form action="">
-    <input name="playerName" type="hidden" value="<?=$name?>">
-    <a><button>Next</button></a>
-</form>
+    <form action="">
+        <input name="playerName" type="hidden" value="<?=$name?>">
+        <a><button>Next</button></a>
+    </form>
+</main>
 
 </body>
 </html>
